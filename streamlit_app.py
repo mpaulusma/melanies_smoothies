ab65192a796aae057f735687c7f316f83d0dd67a
Mick Paulusma <mick.paulusma@gmail.com>
2024-12-21 15:39:31 +0000

    Update streamlit_app.py
    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,15 +44,10 @@ if ingredients_list:
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string + """', '""" + title + """')"""
-
+    
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order!')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-    
         st.success('Your Smoothie is ordered!', icon="✅")
-
-
-
-                                            
