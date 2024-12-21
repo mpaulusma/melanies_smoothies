ffb109656acfa397cfba5a4e1f4c95ad3b9cac40
Mick Paulusma <mick.paulusma@gmail.com>
2024-12-21 11:00:58 +0000

    Update streamlit_app.py
    Missing quotes in cnx string

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 title = st.text_input("Name on smoothie:")
 st.write("The name on your smoothie will be: ", title)
 
-cnx = st.connection("snowflake)
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_data_frame = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe (data = my_data_frame, use_container_width=True)
